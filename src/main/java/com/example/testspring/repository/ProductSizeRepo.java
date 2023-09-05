8c25bbecc4493a69733dff7c0bb5ecd7349885b9
DanhDev111 <tducanh789@gmail.com>
2023-09-05 14:22:55 +0700

    del some unnecessary libraries

--- src/main/java/com/example/testspring/repository/ProductSizeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.testspring.repository;
-
-import com.example.testspring.entity.ProductSize;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductSizeRepo extends JpaRepository<ProductSize,Integer> {
-
-}
